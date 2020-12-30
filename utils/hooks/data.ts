67405091cb8054a335f056bf84710cac83da0352
Leonardo Petrucci <leonardo@creativiii.com>
2020-12-30 16:30:19 +0000

    🩹 Broke import

--- utils/hooks/data.ts
+++ utils/hooks/data.ts
@@ -1,5 +1,5 @@
 import request, { gql } from 'graphql-request';
-import { QueryResult, useQuery } from 'react-query';
+import { QueryObserverResult, useQuery } from 'react-query';
 import { endpoint } from '../../pages/_app';
 
 export interface ServerObjectInterface {
@@ -52,7 +52,7 @@ export async function getServers(
 
 export const useServers = (
   date?: string
-): QueryResult<ServerObjectInterface[], unknown> =>
+): QueryObserverResult<ServerObjectInterface[], unknown> =>
   useQuery('servers', async () => {
     const servers = await getServers(date);
     return servers;
@@ -60,7 +60,7 @@ export const useServers = (
 
 export const useServer = (
   id: string
-): QueryResult<ServerObjectInterface, unknown> =>
+): QueryObserverResult<ServerObjectInterface, unknown> =>
   useQuery('server', async () => {
     const server = await getServer(id);
     return server;
