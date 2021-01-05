e97a59fe1400111decfab36d5ed6bde147e895ba
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-05 19:33:17 +0000

    🏷️ Forgot return type

--- components/forms/cover/cover.tsx
+++ components/forms/cover/cover.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Input from '../input';
 
-const Cover = () => (
+const Cover = (): JSX.Element => (
   <Input type="text" name="cover" placeholder="http://example.com/cover.jpg" />
 );
 
