5b13f882cd1740c37ad539fb85702b488d784ff2
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-10 18:03:17 +0000

    ✨ Added button to post page

--- components/navigation/top/top.tsx
+++ components/navigation/top/top.tsx
@@ -1,22 +1,30 @@
 import Button from 'components/button';
+import { useRouter } from 'next/router';
 import React from 'react';
 import Auth from '../auth/auth';
 
-const Top = (): JSX.Element => (
-  <nav
-    style={{ maxWidth: '1152px' }}
-    className="w-full flex justify-between items-center px-4 py-2 border-b border-gray-100 text-sm text-gray-500"
-  >
-    <div>Search</div>
-    <div className="flex">
-      <div className="mr-4">
-        <Button type="secondary" size="small">
-          Aggiungi server
-        </Button>
+const Top = (): JSX.Element => {
+  const router = useRouter();
+  return (
+    <nav
+      style={{ maxWidth: '1152px' }}
+      className="w-full flex justify-between items-center px-4 py-2 border-b border-gray-100 text-sm text-gray-500"
+    >
+      <div>Search</div>
+      <div className="flex">
+        <div className="mr-4">
+          <Button
+            type="secondary"
+            size="small"
+            onClick={() => router.push('/server/add')}
+          >
+            Aggiungi server
+          </Button>
+        </div>
+        <Auth />
       </div>
-      <Auth />
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Top;
