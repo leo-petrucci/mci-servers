c48052ab4cab1070c63779082c122afdafb864ed
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-16 23:58:12 +0000

    🔧 Changed env var names

--- components/navigation/auth/auth.tsx
+++ components/navigation/auth/auth.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { useUser } from 'utils/hooks/useUser';
 
-const redirectUrl = `${process.env.NEXT_PUBLIC_REDIRECT_URL}`;
+const redirectUrl = `${process.env.REDIRECT_URL}`;
 
 const Auth = (): JSX.Element => {
   const router = useRouter();
